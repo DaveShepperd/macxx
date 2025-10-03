8b36706f0437b3e33b74fcf4ecb669051af6fdac
Dave Shepperd <dshepperd7@gmail.com>
2025-10-02 17:19:21 -0700

    I think the tests suites are finished. Good enough anyway. Version is 13.04.

--- opcas.c
+++ opcas.c
@@ -67,7 +67,7 @@ char macxx_nibbles_long = 8;
 uint32_t macxx_edm_default = ED_LC|ED_GBL|ED_TRUNC; /* default edmask */
 uint32_t macxx_lm_default = ~(LIST_ME|LIST_MEB|LIST_MES|LIST_LD|LIST_COD|LIST_OCT); /* default list mask */
 
-int current_radix = 16;     /* default the radix to hex */
+int current_radix = 10;     /* default the radix to decimal */
 char expr_open = '(';       /* char that opens an expression */
 char expr_close = ')';      /* char that closes an expression */
 /* char expr_escape = '^'; */     /* char that escapes a unary expression term */
--- version.h
+++ version.h
@@ -19,6 +19,6 @@
 #ifndef _VERSION_H_
 #define _VERSION_H_ 1
 
-char *macxx_version = "v13.03";
+char *macxx_version = "v13.04";
 
 #endif /* _VERSION_H_ */
